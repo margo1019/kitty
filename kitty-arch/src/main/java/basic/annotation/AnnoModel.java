7a2f9807f2169271a93f851b276ad8bf597cf1b4
ljy <liujiayong@tsinghua.org.cn>
2017-05-24 14:19:14 +0800

    t

---
+++ kitty-arch/src/main/java/basic/annotation/AnnoModel.java
@@ -0,0 +1,12 @@
+package basic.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface AnnoModel {
+
+    String name() default "_name";
+
+    int intAN() default 0;
+}
