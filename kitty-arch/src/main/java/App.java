add3fd482fce094de89e288ff98b550fb706b6c5
LiuJY <tt@tt.com>
2017-06-06 22:47:30 +0800

    t

--- kitty-arch/src/main/java/App.java
+++ kitty-arch/src/main/java/App.java
@@ -1,9 +1,6 @@
-import org.slf4j.Logger;
-
 import java.io.PrintStream;
 
 public class App {
-    Logger logger;
 
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
