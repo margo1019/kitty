5724f72d832ca978d50ac8626e34a584665647a7
yinfei <yinfei@taobao.com>
2017-06-13 00:15:38 +0800

    del web dal

--- kitty-arch/src/main/java/proxy/spring/SpringAopProxy.java
+++ kitty-arch/src/main/java/proxy/spring/SpringAopProxy.java
@@ -1,10 +1,13 @@
 package proxy.spring;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.aop.framework.ProxyFactory;
 import proxy.Piper;
 import proxy.PiperImplClean;
 
 public class SpringAopProxy {
+    protected static final Log logger = LogFactory.getLog(SpringAopProxy.class);
 
     public static void testBeforeAfter () {
         ProxyFactory pf = new ProxyFactory();
