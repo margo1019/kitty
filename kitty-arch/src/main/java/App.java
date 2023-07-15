e97b2e1441a4b3b6c9e3c6511a8b8ef3693fe3bd
margo1019 <Liujy0214>
2023-07-15 13:03:17 +0800

    123

--- kitty-arch/src/main/java/App.java
+++ kitty-arch/src/main/java/App.java
@@ -1,12 +1,7 @@
-import java.io.PrintStream;
 
 public class App {
-
     public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
-        long laps = System.currentTimeMillis() - 1459920556643L;
-        double hours = laps*1.0 / 1000/3600/24;
-        System.out.println(hours);
+        System.out.println("hi2");
     }
 
 }
